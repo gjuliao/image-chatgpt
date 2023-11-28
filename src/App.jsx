b09c2f03e4555adb8e45f5dbf052a74035101e59
gjuliao <gjuliao32@gmail.com>
2023-11-28 15:45:40 -0500

    Fixed bug and added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,56 @@
 import './App.css'
+import { OpenAI } from 'openai';
+import { useState } from 'react';
 
 
 
 function App() {
 
- 
-  console.log(import.meta.env.VITE_OPENAI_API_KEY);
+  const [prompt, setPrompt] = useState('');
+  const [image, setImage] = useState('');
+
+  const generateImage = async () => {
+    try {
+
+      const openai = new OpenAI({
+        apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+        dangerouslyAllowBrowser: true,
+      });
+
+      const res = await openai.images.generate({
+        model:"dall-e-2",
+          prompt: prompt,
+          n: 1,
+          size: '1024x1024',
+      });
+
+      console.log(res);
+
+      const image = await res.data[0].url;
+
+      setImage(image);
+    } catch (error) {
+      if (error.response) {
+        // The error has a response property, so you can access status and data
+        console.error("Error generating image:", error.response.status, error.response.data);
+      } else {
+        // The error does not have a response property, handle it accordingly
+        console.error("Error generating image:", error.message);
+      }
+    }
+  };
+
+  // const setImage = () => {
+
+  // }
 
   return (
     <div className='container'>
 
       <h2>Create an Image using Chat-GPT</h2>
-      <input />
-      <button>Generate Image</button>
+      <input className='user-input' onChange={(e) => setPrompt(e.target.value)} placeholder='Describe what you want to generate' />
+      <button className='btn' onClick={generateImage}>Generate Image</button>
+      {image.length > 0 ? <img className='gen-image' src={image} alt="Generated image" /> : <></>}
     </div>
   )
 }
