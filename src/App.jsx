8dd0c76be7e5617ea2ae0403e100fafd3eab6250
gjuliao <gjuliao32@gmail.com>
2023-11-28 11:19:33 -0500

    added openai package

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 
+
+
 function App() {
 
+ 
+  console.log(import.meta.env.VITE_OPENAI_API_KEY);
+
   return (
-    <>
-      <h2>hello world</h2>
-    </>
+    <div className='container'>
+
+      <h2>Create an Image using Chat-GPT</h2>
+      <input />
+      <button>Generate Image</button>
+    </div>
   )
 }
 
